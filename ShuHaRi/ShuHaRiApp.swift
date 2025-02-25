e2009fecc3ab24d5a0cd5fd253c9e53ea8d107f0
YukiYotsu <yy.yotsumoto@icloud.com>
2025-02-25 09:24:08 +0200

    Initial Commit

---
+++ ShuHaRi/ShuHaRiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuHaRiApp.swift
+//  ShuHaRi
+//
+//  Created by takumi on 2025/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ShuHaRiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShuHaRiTests/ShuHaRiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShuHaRiTests.swift
+//  ShuHaRiTests
+//
+//  Created by takumi on 2025/02/25.
+//
+
+import Testing
+@testable import ShuHaRi
+
+struct ShuHaRiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
