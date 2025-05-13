fa885af875d15c48db6a5dcc8d2ea1ceebe07d66
YukiYotsu <yy.yotsumoto@icloud.com>
2025-05-13 18:13:36 +0300

    lighter

--- ShuHaRi/ShuHaRiApp.swift
+++ ShuHaRi/ShuHaRiApp.swift
@@ -10,24 +10,39 @@ import UserNotifications
 
 @main
 struct ShuHaRiApp: App {
-    // 通知設定
-    init() {
-            requestNotificationPermission()
-        }
+
+//            // フォーマットとデータ定義
+//            let formatter = DateFormatter()
+//            formatter.dateFormat = "yyyy/MM/dd HH:mm"
+//
+//            // 通知スケジューリングデータ
+//            let notificationData: [(String, String, String, String)] = [
+//                ("2025/05/24 10:30", "🧠Trustware: Buliding Ethical Intelligence in the AI Age", "Yuki is speaking at 📍Hall.","🐧Republic of Penguins"),
+//                ("2025/05/24 11:30", "City of Legacy -what should have remained in Berlin and Tokyo-", "The new workshop is opening at 📍Hall", "City of Legacy -what should have remained in Berlin and Tokyo-"),
+//                ("2025/05/24 13:00", "🎎The Art of Japanese Dance", "Experience Nihon Buyo🇯🇵 at 📍Hall","The Art of Japanese Dance: An Introduction to Nihon Buyo"),
+//                ("2025/05/24 14:30", "🎹The sound of light", "Hikari no Oto at Hall", "Hikari no oto -- The sound of light"),
+//                ("2025/05/24 16:45", "🕺Dancing the Waves of Emotion", "Hikari no Oto at 📍Hall", "Dancing the Waves of Emotion"),
+//                ("2025/05/24 10:00", "🍵Teeraum", "Japanese tea café at 📍Networking Lounge", "Teeraum"),
+//                ("2025/05/24 17:30", "🍵Teeraum", "It's time for last orders.", "Teeraum"),
+//                ("2025/05/24 11:00", "🥢Ichiju-Sansai", "Experience Japanese food at 📍Networking Lounge", "Ichiju-Sansai"),
+//                ("2025/05/24 13:30", "🥢Ichiju-Sansai", "It's time for last orders.", "Ichiju-Sansai"),
+//                ("2025/05/24 14:30", "Takoyaki", "Street Snack at 📍Networking Lounge", "Takoyaki"),
+//                ("2025/05/24 16:30", "Takoyaki", "It has been closed.", "Takoyaki"),
+//                ("2025/05/24 10:45", "Takibi Storytelling❶", "Sara is speaking at 📍Courtyard", "Takibi storytelling"),
+//                ("2025/05/24 16:00", "Takibi Storytelling❷", "Sara is speaking at 📍Courtyard", "Takibi storytelling"),
+//                ("2025/05/24 15:30", "🎧Tiny theater", "Let's listen to Sky Jazz at 📍Courtyard", "Tiny Theater -- SKY JAZZ: First Flight-日独LIVE"),
+//                ("2025/05/24 17:00", "🥋Physical Performance Talkshow", "Talkshow at 📍Courtyard", ""),
+//                ("2025/05/24 11:30", "📚Next Step Together❶", "Join discussion at 📍Information Lounge", "Next Step Together"),
+//                ("2025/05/24 15:30", "📚Next Step Together❷", "Join discussion at 📍Information Lounge", "Next Step Together"),
+//                ("2025/05/24 14:30", "In the Frame", "Let's talk at 📍Seminar Room 1&2.", "In the Frame"),
+//            
+//            ]
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
     
-    // 通知許可アラートを出す
-    func requestNotificationPermission() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if let error = error {
-                print("Notification permission error: \(error.localizedDescription)")
-            }
-        }
-    }
 }
-
 
